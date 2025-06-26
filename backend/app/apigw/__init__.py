228ff6e7dc196abd7821f2d9e03b78b678fc750f
Camilo <juancamilo.vs206@gmail.com>
2025-06-25 19:28:14 -0500

    modify docker compose file to include Wilma

--- backend/app/apigw/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('apigw', __name__)
-
-# This is necessary to avoid circular imports
-from app.apigw import routes
--- backend/app/apigw/routes.py
+++
@@ -1,8 +0,0 @@
-from app.apigw import bp
-
-@bp.route('/', methods=['GET'])
-def index():
-  """
-  Health check endpoint to verify if the API Gateway is running.
-  """
-  return 'API Gateway is running!'
